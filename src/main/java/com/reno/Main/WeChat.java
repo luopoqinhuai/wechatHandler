75ae326bf9bd96a88d22ba8929da3365241c742d
luopoqinhuai <qxl19920213@163.com>
2016-08-05 21:22:19 +0800

    Initial commit

---
+++ src/main/java/com/reno/Main/WeChat.java
@@ -0,0 +1,20 @@
+package com.reno.Main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Created by Reno on 2016/8/5.
+ */
+@SpringBootApplication
+public class WeChat {
+    public static void main(String[] args) {
+        SpringApplication.run(WeChat.class, args);
+    }
+}
+
+
+
+
+
+
+
